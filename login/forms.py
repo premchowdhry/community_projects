8a95201dd322ebbea5ded97a52eeb36f61e87891
shindexro <shindexro@gmail.com>
2018-10-11 18:33:46 +0100

    Remove unnecessary fields in signup form

--- login/forms.py
+++ login/forms.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
+    # first_name = forms.CharField(max_length=30)
+    # last_name = forms.CharField(max_length=30)
     email = forms.EmailField(max_length=254)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2', )
