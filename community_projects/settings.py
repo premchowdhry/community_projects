7789b92fdeaebc88056bc74a0134464db400262e
shindexro <shindexro@gmail.com>
2018-10-12 21:26:50 +0100

    Add google+ login option

--- community_projects/settings.py
+++ community_projects/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'login.apps.LoginConfig',
     'post.apps.PostConfig',
     'crispy_forms',
-    # 'social_django',
+    'social_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,12 +44,13 @@ INSTALLED_APPS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
- # 'social_core.backends.open_id.OpenIdAuth',
- # 'social_core.backends.google.GoogleOpenId',
- # 'social_core.backends.google.GoogleOAuth2',
+ 'social_core.backends.open_id.OpenIdAuth',
+ 'social_core.backends.google.GoogleOpenId',
+ 'social_core.backends.google.GoogleOAuth2',
+ 'social_core.backends.facebook.FacebookOAuth2',
  'django.contrib.auth.backends.ModelBackend',
 )
-SOCIAL_AUTH_URL_NAMESPACE = 'social'
+# SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -155,32 +156,10 @@ DEFAULT_FROM_EMAIL = 'dodev4ever@gmail.com'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-SOCIAL_AUTH_PIPELINE = (
-    'social.pipeline.social_auth.social_details',
-    'social.pipeline.social_auth.social_uid',
-    'social.pipeline.social_auth.auth_allowed',
-    'social.pipeline.social_auth.social_user',
-    'social.pipeline.user.get_username',
-    'social.pipeline.user.create_user',
-    'social.pipeline.social_auth.associate_user',
-    'social.pipeline.debug.debug',
-    'social.pipeline.social_auth.load_extra_data',
-    'social.pipeline.user.user_details',
-    'social.pipeline.debug.debug',
-)
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_IGNORE_DEFAULT_SCOPE = True
-SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
-'https://www.googleapis.com/auth/userinfo.email',
-'https://www.googleapis.com/auth/userinfo.profile'
-]
 # Google+ SignIn (google-plus)
-SOCIAL_AUTH_GOOGLE_PLUS_IGNORE_DEFAULT_SCOPE = True
-SOCIAL_AUTH_GOOGLE_PLUS_SCOPE = [
-'https://www.googleapis.com/auth/plus.login',
-'https://www.googleapis.com/auth/userinfo.email',
-'https://www.googleapis.com/auth/userinfo.profile'
-]
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '407132811738-519f2tnfnqkhig0qci9ltevvp0f185cu.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'OAA6-VMhUO8UtgZJNwe96rnR'
-LOGIN_URL = '/account/login/'
+# Follow guide on
+# https://fosstack.com/how-to-add-google-authentication-in-django/
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '245751261398-qd560b4sekmvb8oe6tkhtt6qemfkhnqf.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'mzD90PO8Ni5FmUUHxqUfal6g'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'find_post'
