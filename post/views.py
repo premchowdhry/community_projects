33865cd18a1f513a96d30e074b4bf45b7d9b100f
shindexro <shindexro@gmail.com>
2018-10-15 17:08:44 +0100

    Create requirement list

--- post/views.py
+++ post/views.py
@@ -3,15 +3,25 @@ from .models import Post
 
 def find(request):
     context = {
+        'user': request.user,
         'posts': Post.objects.all(),
     }
     return render(request, 'post/find.html', context)
 
 def post(request):
+    context = {
+        'user': request.user
+    }
     return render(request, 'post/post.html')
 
 def dashboard(request):
+    context = {
+        'user': request.user
+    }
     return render(request, 'post/dashboard.html')
 
 def profile(request):
+    context = {
+        'user': request.user
+    }
     return render(request, 'post/profile.html')
